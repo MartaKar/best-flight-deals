969084950b02282f9f3fc0116da4a8b505f86aab
Marta <martakaras1@gmail.com>
2022-09-06 13:16:34 +0200

    Initial commit

---
+++ data_manager.py
@@ -0,0 +1,28 @@
+import requests
+
+SHEETY_PRICES_ENDPOINT = "https://api.sheety.co/520c6a1ec6d76be2a12dd25cd5b84f64/flightDeals/prices"
+
+
+class DataManager:
+
+    def __init__(self):
+        self.destination_data = {}
+
+    def get_destination_data(self):
+        response = requests.get(url=SHEETY_PRICES_ENDPOINT)
+        data = response.json()
+        self.destination_data = data["prices"]
+        return self.destination_data
+
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+            response = requests.put(
+                url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}",
+                json=new_data
+            )
+            print(response)
